ccec670f5a15acc1395c5665da40eb8ce322b1a9
rundimeco <rundimeco@gmail.com>
2019-04-25 15:32:57 +0200

    scorer improved

--- scorer.py
+++ scorer.py
@@ -31,7 +31,7 @@ def compute_results(dic, has_date, verbose):
   acc_data = [0, 0]
   out =""
   if verbose == True:
-    print "  CLASSE\tPrecision\tRecall\tF-measure\tTP\tFP\tFN"
+    print("  CLASSE\tPrecision\tRecall\tF-measure\tTP\tFP\tFN")
   l_classes = sorted(dic.keys())
   for classe in l_classes:
     scores = dic[classe]
@@ -48,8 +48,7 @@ def compute_results(dic, has_date, verbose):
     acc_data[1]+=FP+TP
     all_F.append(F1)
     if verbose==True:
-      print "  '%s'"%classe[:10]+"\t"+"\t".join([str(round(x,4)) for x in [P,R, F1]])+"\t",
-      print "\t".join([str(scores[x]) for x in ["TP", "FP",  "FN"]])
+      print("  '%s'"%classe[:10]+"\t"+"\t".join([str(round(x,4)) for x in [P,R, F1]])+"\t"+"\t".join([str(scores[x]) for x in ["TP", "FP",  "FN"]]))
   if acc_data[0]>0:
     accuracy = round(acc_data[0]/acc_data[1], 4)
   else:
@@ -58,17 +57,16 @@ def compute_results(dic, has_date, verbose):
 #  dic = {"Macro-F1": round(moyenne(all_F), 4)}
 #  dic = {"Accuracy": accuracy}
   if verbose==True:
-    print "  Accuracy:", accuracy, 
-    print "  Macro F1:", round(moyenne(all_F), 4)
+    print("  Accuracy : %f"%accuracy)
+    print("  Macro F1 : %f"%round(moyenne(all_F), 4))
   return dic
-#  print out
 
 def get_scores(ref, pred, has_date, verbose):
   classes_names = set(ref.values())
   d_classes = init_data_struct(classes_names)
   missing = []
   eval_dates = {"dist":0, "dist_pond":0, "sim_gauss":0}
-  for ID, classe in ref.iteritems():
+  for ID, classe in ref.items():
     if ID not in pred:
       missing.append(ID)
       continue
@@ -84,23 +82,21 @@ def get_scores(ref, pred, has_date, verbose):
         eval_dates["dist"]+=ecart_abs/len(pred)
         eval_dates["dist_pond"]+=ecart_abs*ecart_abs/len(pred)
         sim_gauss = (2.72818**(-(3.1415/10)*ecart_abs*ecart_abs))
-#        print(sim_gauss, classe, pred[ID])
-#        DDD = raw_input("next?")
         eval_dates["sim_gauss"]+=sim_gauss/len(pred)
   scores = compute_results(d_classes, has_date,verbose)
   if has_date:
     print(eval_dates)
     scores["sim_gauss"] = round(eval_dates["sim_gauss"], 4)
-  if missing>0:
-    print "Missing : %s"%str(len(missing))
+  if len(missing)>0:
+    print("Missing : %s"%str(len(missing)))
   return scores, eval_dates
-
-print "Usage : ARG1=GOLD_FILE ARG2= PATH_RESULTS"
+print("-"*20)
+print("Usage : ARG1=GOLD_FILE ARG2= PATH_RESULTS")
+print("-"*20)
 if len(sys.argv)!=3:
   print("ARGS", sys.argv)
   exit()
 
-#liste_results = glob.glob(sys.argv[2]+"*/*/*/*")
 liste_results = glob.glob(sys.argv[2]+"*")
 ref = parse_file(sys.argv[1])
 
@@ -110,27 +106,14 @@ has_date = False#for adapting to regression cases
 verbose = False
 
 for result_file in liste_results:
-  print "\n Processing %s"%result_file[-80:]
+  print("\n Processing %s"%result_file[-80:])
   pred = parse_file(result_file)
   scores, scores_date = get_scores(ref, pred, has_date, verbose)
   if verbose==False:
     print(scores)
   maxlen = re.findall("maxlen-([0-9])", result_file)[0]
   minlen = re.findall("minlen-([0-9])", result_file)[0]
-#  if "modern" in result_file:lg ="Moderne"
-#  else:lg="Diplomatique"
-#  if "NoMarkup" in result_file:mk = "Brut"
-#  else: mk = "Clean"
-#  if "grams-T" in result_file:tok = "Motifs"
-#  else: tok = "N-grammes"
-#  if "words-T" in result_file:typ="mots"
-#  else: typ="caracteres"
-#  D.setdefault(lg, {})
-#  tup = tuple([lg, mk, tok, typ])
-#  legende = "__".join([x.encode("utf-8") for x in tup])
   if int(minlen)>1:continue
-#  D[lg].setdefault(legende,[{},{},{},{},{},{},{}])
-#  D[lg][legende][int(maxlen)-1] = scores
   parametres = re.findall("([a-z]*)-([A-Z][a-z]*|[0-9][0-9]*)", result_file)
   print(scores)
   print(parametres)
@@ -145,23 +128,23 @@ for result_file in liste_results:
   dic_pyplot.setdefault(name_config, [])
   dic_pyplot[name_config].append([parametres[x] for x in ["minlen","maxlen","minsup","maxsup"]]+list([scores[x] for x in etiq_scores]))
 
-dic_pyplot = {x:sorted(y) for x,y in dic_pyplot.iteritems()}
+dic_pyplot = {x:sorted(y) for x,y in dic_pyplot.items()}
 dic_name = "pyplot.json"
 w = open(dic_name, "w")
 w.write(json.dumps(dic_pyplot, indent = 2))
 w.close()
 print("Output written in %s"%dic_name)
 
-for lg, Dic in D.iteritems():
+for lg, Dic in D.items():
   legendes = []
-  print Dic.keys()
-  for tup, scores in Dic.iteritems():
+  print(Dic.keys())
+  for tup, scores in Dic.items():
     nom = "_".join(list(tup))
     y = []
-    print scores
+    print(scores)
     for dic in scores:
       if len(dic)=={}:continue
-      for a,b in dic.iteritems():
+      for a,b in dic.items():
         y.append(b)
     X = range(1, len(y)+1)
     plt.plot(X,y)
